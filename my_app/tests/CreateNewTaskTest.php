3d0c31e73cce8ab19229b80bb5051129590e732f
Leszek <leslawkula@gmail.com>
2021-09-16 22:16:34 +0200

    create new task negative unit tests

--- my_app/tests/CreateNewTaskTest.php
+++ my_app/tests/CreateNewTaskTest.php
@@ -1,1 +1,5 @@
 #test pozytywny - wybrana kategoria, wpisana nazwa, wpisany termin
+
+#test negatywny - nie wybrana kategoria, wpisana nazwa, wpisany termin
+#test negatywny - wybrana kategoria, nie wpisana nazwa, wpisany termin
+#test negatywny - wybrana kategoria, wpisana nazwa, nie wpisany termin
